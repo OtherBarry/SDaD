b263efbfa75a8d68235559b6e188f7f40c3d5a35
OtherBarry <charlie.wilson4@gmail.com>
2015-02-20 23:15:07 +1100

    Halfway bak to norma

--- Translator/Translator/varStorage.vb
+++
@@ -1,7 +0,0 @@
-﻿Module varStorage
-    Public usrLanguage As String
-    Public usrName As String
-    Public lngPhrase As String
-    Public lngTranslator As String
-    Public lngtranslatee As String
-End Module
